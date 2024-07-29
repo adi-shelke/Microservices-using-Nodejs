4cebdda88cb734c2509e1fab83052a5ace489454
Adinath Shelke <shelkeadinath3@gmail.com>
2024-07-29 23:11:11 +0530

    added query service

---
+++ event-bus/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+const app = express();
+app.use(express.json());
+
+app.post("/events", async (req, res) => {
+
+  const event = req.body;
+
+   axios.post("http://localhost:4000/events", event);
+   axios.post("http://localhost:4001/events", event);
+   axios.post("http://localhost:4002/events", event);
+
+  res.send({ status: "OK" });
+});
+
+
+app.listen(4005, () => {
+    console.log("Event bus listening on 4005");
+    }
+);
+
+
